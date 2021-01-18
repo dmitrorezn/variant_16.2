6ed6ad83ece5bf2f58a5d921e7afc4dd05824190
evgeniy132 <evgeniy132@gmail.com>
2021-01-18 13:03:03 +0200

    done

---
+++ Book/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Book.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Book.Program.BooksContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(Book.Program.BooksContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ Book/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace Book
+{
+    class Program
+    {
+        public class Book
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public int AuthorId { get; set; }
+            public List<BookReview> BookReviews { get; set; }
+        }
+        public class BookReview
+        {
+            public int Id { get; set; }
+            public string ReviewerName { get; set; }
+            public int Rating { get; set; }
+            public Book Book { get; set; }
+        }
+        public class Author
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+        public class BooksContext : DbContext
+        {
+            public DbSet<Book> Books { get; set; }
+            public DbSet<BookReview> BookReviews { get; set; }
+            public DbSet<Author> Authors { get; set; }
+            protected override void OnModelCreating(DbModelBuilder modelBuilder)
+            {
+                modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+                modelBuilder.Entity<Book>().Property(b => b.Title).IsRequired().HasMaxLength(512);
+                modelBuilder.Entity<BookReview>().Property(r => r.ReviewerName).IsRequired();
+                modelBuilder.Entity<Author>().Property(a => a.Name).IsRequired();
+
+            }
+        }
+
+        public class AuthorRepository
+        {
+            public List<Author> GetAuthorsByRating(int Rating)
+            {
+                using (BooksContext ctx = new BooksContext())
+                {
+                    List<Author> authors = new List<Author>();
+                    var a = ctx.Authors.Join(ctx.Books,
+                        f => f.Id,
+                        b => b.AuthorId,
+                        (f, b) => new
+                        {
+                            Id = f.Id,
+                            Name = f.Name,
+                            BR = b.BookReviews.Average(p => p.Rating)
+                        });
+                    foreach (var br in a)
+                    {
+                        var res = br.BR;
+                        if (res >= Rating)
+
+                        {
+                            Author author = new Author()
+                            {
+                                Id = br.Id,
+                                Name = br.Name
+                            };
+                            authors.Add(author);
+                        }
+
+                    }
+                    return authors;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            var repo = new AuthorRepository();
+            var authors = repo.GetAuthorsByRating(3);
+            foreach (var author in authors)
+                Console.WriteLine(author.Name);
+        }
+    }
+}
